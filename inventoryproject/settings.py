5f5bddcecdf05c1a0199d9ff33b45f309c0c0a45
samezzz <95895530+samezzz@users.noreply.github.com>
2022-12-03 03:59:48 +0000

    environment variables

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w*pfrf48ga-!b%8wych6ul=pe0!$267kz2&=z)1d2i%b4s18c-'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG" cast=bool)
 
 ALLOWED_HOSTS = []
 
